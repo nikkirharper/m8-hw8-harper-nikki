5dba9d4fbd04fa5467f848e6ad74c4cde8f39144
Nikki Harper <nicoleharper@ufl.edu>
2021-03-06 13:42:06 -0700

    Add css transition on hover to products

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// It is always helpful to use comments in your code!
+// Navigation sub-menu transition
+
+var button1 = document.getElementById("button1"); 
+var wrapper1 = document.getElementById("products-wrapper"); 
+
+function showWrapper() {
+    wrapper1.classList.add("show");
+}
+
+function hideWrapper() {
+    wrapper1.classList.remove("show"); 
+}
+
+button1.addEventListener("mouseover", showWrapper);
+button1.addEventListener("mouseleave", hideWrapper); 
